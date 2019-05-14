e1403da751fbbc958a21474beaeb9bd0f61173b2
Jeffrey Moore <dmjeffmoore@gmail.com>
2019-05-13 21:57:54 -0700

    Added restaurant input and add button

--- app/src/main/java/com/dmjeffmoore/rist/MainActivity.kt
+++ app/src/main/java/com/dmjeffmoore/rist/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.dmjeffmoore.rist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.dmjeffmoore.rist.MESSSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun addRestaurant(view: View) {
+        val restaurantText = findViewById<EditText>(R.id.restaurant_input)
+        val restaurant = restaurantText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, restaurant)
+        }
+        startActivity(intent)
+    }
+
 }
